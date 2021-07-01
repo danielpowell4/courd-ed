f488d0ae4f72eed5656427ef38fb27127b88ce98
Dan <danielpowell12@msn.com>
2021-07-01 12:04:53 -0600

    v1 implementation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import courdoroy from './courdoroy.jpg';
+
+const GRID_SIZE = 12;
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ backgroundColor: "blue", padding: 10 }}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Courdoroy Ed</h1>
       </header>
+      <main
+      style={{display: "grid",
+    gridTemplateColumns: `repeat(${GRID_SIZE}, 100px)`,
+    margin: "0 auto 2rem",
+    maxWidth: GRID_SIZE * 100,
+    width: "100%"}}>
+      {Array(GRID_SIZE).fill("").map((_, rowIndex) => Array(GRID_SIZE).fill("").map((_, columnIndex) =>{
+          let style = {
+            animation: `hueRotation 3.5s linear 200ms infinite`,
+          }
+          style[`-webkit-filter`] = `hue-rotate(${(rowIndex + columnIndex) * 15}deg)`
+          style[`filter`] = `hue-rotate(${(rowIndex + columnIndex) * 15}deg)`
+
+          let rotation = 0;
+
+          /* odd row, even column */
+          if (rowIndex % 2 && columnIndex % 2 === 0) {
+            rotation = 90
+          }
+
+          /* even row, odd column */
+          if ((rowIndex % 2 === 0) && columnIndex % 2) {
+            rotation = 90
+          }
+
+
+          style.transform = `rotate(${rotation}deg)`
+
+          return (
+            <img key={`${rowIndex}-${columnIndex}`} src={courdoroy} alt="Courdoroy" height={100} width={100} style={style}/>
+          )
+        } ))}
+      </main>
+      <p>Home of the High Five 🙏</p>
     </div>
   );
 }
